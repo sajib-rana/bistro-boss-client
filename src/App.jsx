3d05eb4da6c81946dba31d5fb79297f0578b9770
sajib-rana <sajibrana444@gmail.com>
2024-03-31 15:52:51 +0600

    intial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <button class="btn btn-primary">One</button>
+      <button class="btn btn-secondary">Two</button>
+      <button class="btn btn-accent btn-outline">Three</button>
+    </>
+  );
+}
+
+export default App
