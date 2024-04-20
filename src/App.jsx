c2859409977ab9522b39da8cc9e2297ae48b870f
sajib-rana <sajibrana444@gmail.com>
2024-04-21 00:35:48 +0600

    router

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,8 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <button class="btn btn-primary">One</button>
-      <button class="btn btn-secondary">Two</button>
-      <button class="btn btn-accent btn-outline">Three</button>
+      
+      
     </>
   );
 }
